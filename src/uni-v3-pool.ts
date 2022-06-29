645348b50fffafbcf3b57735659c23b0d13e032a
David Mihal <david@dmihal.com>
2022-06-29 23:24:16 +0100

    Add Optimism

--- src/uni-v3-pool.ts
+++ src/uni-v3-pool.ts
@@ -1,14 +1,39 @@
-import { Address, BigDecimal, BigInt, log } from "@graphprotocol/graph-ts"
+import { Address, BigDecimal, BigInt, dataSource, ethereum, log } from "@graphprotocol/graph-ts"
 import { Swap } from "../generated/UniV3Pool/UniV3Pool"
 import { ArbGasPrecompile } from "../generated/UniV3Pool/ArbGasPrecompile"
+import { OptiGasPrecompile } from "../generated/UniV3Pool/OptiGasPrecompile"
 import { DayStat, GlobalStat } from "../generated/schema"
 
 let router = Address.fromString('0xe592427a0aece92de3edee1f18e0157c05861564')
 let arbGasPrecompile = Address.fromString('0x000000000000000000000000000000000000006C')
+let optiGasPrecompile = Address.fromString('0x420000000000000000000000000000000000000F')
 
 let eighteenDecimals = BigInt.fromI32(10).pow(18).toBigDecimal()
 let twelveDecimals = BigInt.fromI32(10).pow(12).toBigDecimal()
 let ONE_DAY = 86400
+
+function getGasPrice(event: ethereum.Event): BigInt {
+  let chainId = dataSource.network()
+
+  if (chainId == 'arbitrum-one') {
+    return ArbGasPrecompile.bind(arbGasPrecompile).getPricesInWei().getValue5()
+  }
+
+  return event.transaction.gasPrice
+}
+
+function getEthFee(event: ethereum.Event): BigDecimal {
+  let chainId = dataSource.network()
+  let gasPrice = getGasPrice(event)
+
+  if (chainId == 'optimism') {
+    let l2Fee = event.receipt!.gasUsed.times(gasPrice)
+    let l1Fee = OptiGasPrecompile.bind(optiGasPrecompile).getL1Fee(event.transaction.input)
+    return l2Fee.plus(l1Fee).divDecimal(eighteenDecimals)
+  }
+
+  return event.receipt!.gasUsed.times(gasPrice).divDecimal(eighteenDecimals)
+}
 
 export function handleSwap(event: Swap): void {
   if (event.transaction.to != router) {
@@ -23,10 +48,10 @@ export function handleSwap(event: Swap): void {
     ethPrice = ethPrice.neg()
   }
 
-  let gasPrice = ArbGasPrecompile.bind(arbGasPrecompile).getPricesInWei().getValue5()
-
-  let ethFee = event.receipt!.gasUsed.times(gasPrice).divDecimal(eighteenDecimals)
+  let ethFee = getEthFee(event)
   let usdFee = ethFee.times(ethPrice)
+
+  log.info("tx {} eth {} usd {}", [event.transaction.hash.toHex(), ethFee.toString(), usdFee.toString()])
 
   let globalStats = GlobalStat.load('1')
   if (!globalStats) {
